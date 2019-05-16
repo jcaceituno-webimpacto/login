f741554080460709b6d01933d2159bfd170315d6
jcaceitunowebimpacto <jcaceituno@webimpacto.es>
2019-05-16 13:03:24 +0200

    Commit 16-05-2019

--- react/Login.js
+++ react/Login.js
@@ -3,13 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUser} from '@fortawesome/free-solid-svg-icons';
 library.add(faUser);
+import styles from './styles.css';
 
 
 export default class Login extends Component {
     render() {
         return (
             <div>
-                <span>Sign In</span><FontAwesomeIcon icon="user"/>
+                <span className={styles.textoLogin}>Sign In</span><FontAwesomeIcon icon="user" className={styles.icono}/>
             </div>
         )
     }
